7bd5d8d49d763155fc9f3bf381f5c394fe9f13b9
Pradnyesh Aglawe <pradnyeshaglawe12@gmail.com>
2020-04-03 16:38:26 +0530

    Showing the Avatar, Connecting the Dots

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,15 +13,7 @@ module.exports.profile = function(req, res){
 
 
 module.exports.update = async function(req, res){
-    // if(req.user.id == req.params.id){
-    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-    //         req.flash('success', 'Updated!');
-    //         return res.redirect('back');
-    //     });
-    // }else{
-    //     req.flash('error', 'Unauthorized');
-    //     return res.status(401).send('Unauthorized');
-    // }
+    
 
     if(req.user.id == req.params.id){
 
